a2ff46c245326fc783ac3b226ba6cc4649bd3356
AlexNaga <an222zd@student.lnu.se>
2018-02-28 09:17:41 +0100

    Init

--- src/routes/github.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const githubController = require('../controllers/github');
-
-router.post('/', githubController.githubWebhook);
-
-module.exports = router;
