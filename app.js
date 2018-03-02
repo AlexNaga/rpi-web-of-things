8e5a4cb91445500208b319dfd59c119d21acf367
AlexNaga <an222zd@student.lnu.se>
2018-03-02 08:37:30 +0100

    Emit data to ws

--- app.js
+++ app.js
@@ -38,40 +38,4 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Setup sensors
-const sensorLib = require('raspi-sensors');
-const refreshTimeInSec = 5;
-
-let BMP180 = new sensorLib.Sensor({
-  type: 'BMP180',
-  address: 0x77
-}, 'temp_pressure_sensor');
-
-let DHT22 = new sensorLib.Sensor({
-  type: 'DHT22',
-  pin: 0X7
-}, 'temp_humidity_sensor');
-
-let TSL2561 = new sensorLib.Sensor({
-  type: 'TSL2561',
-  address: 0X39
-}, 'light_sensor');
-
-// Define a callback
-let cb = (err, data) => {
-  if (err) {
-    console.error("An error occured!");
-    console.error(err.cause);
-    return;
-  }
-
-  console.log(data);
-}
-
-BMP180.fetch(cb);
-DHT22.fetch(cb);
-TSL2561.fetch(cb);
-
-// DHT22.fetchInterval(cb, refreshTimeInSec);
-
 module.exports = app;
--- server.js
+++ server.js
@@ -6,11 +6,4 @@ const server = http.createServer(app);
 
 const io = require('socket.io').listen(server);
 
-io.on('connection', function (socket) {
-  console.log('a user connected');
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
-});
-
 server.listen(port);
