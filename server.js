60d85ef50e3f802574787e2e3d8d51f561987c32
AlexNaga <an222zd@student.lnu.se>
2018-03-02 07:58:13 +0100

    Change order

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const app = require('./app');
 const http = require('http');
-const io = require('socket.io')(http);
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
+
+const io = require('socket.io').listen(server);
 
 io.on('connection', function (socket) {
   console.log('a user connected');
