3a66850ea0c6b2be4167136d0b8193e87f4b5556
AlexNaga <an222zd@student.lnu.se>
2018-03-02 15:56:27 +0100

    fix

--- server.js
+++ server.js
@@ -13,45 +13,47 @@ let BMP180 = new sensorLib.Sensor({
   address: 0x77
 }, 'temp_pressure_sensor');
 
-let DHT22 = new sensorLib.Sensor({
-  type: 'DHT22',
-  pin: 0X7
-}, 'temp_humidity_sensor');
+// let DHT22 = new sensorLib.Sensor({
+//   type: 'DHT22',
+//   pin: 0X7
+// }, 'temp_humidity_sensor');
 
-let TSL2561 = new sensorLib.Sensor({
-  type: 'TSL2561',
-  address: 0X39
-}, 'light_sensor');
+// let TSL2561 = new sensorLib.Sensor({
+//   type: 'TSL2561',
+//   address: 0X39
+// }, 'light_sensor');
 
 BMP180.fetchInterval(function (err, data) {
   if (err) {
     console.error("An error occured: ", err.cause);
     return;
   }
+  console.log(data);
+  
 
   let sensorModel = data.sensor_type;
   io.sockets.emit('bmp180_temp', data);
 }, refreshTimeInSec);
 
-DHT22.fetchInterval(function (err, data) {
-  if (err) {
-    console.error("An error occured: ", err.cause);
-    return;
-  }
+// DHT22.fetchInterval(function (err, data) {
+//   if (err) {
+//     console.error("An error occured: ", err.cause);
+//     return;
+//   }
 
-  let sensorModel = data.sensor_type;
-  io.sockets.emit(sensorModel, data);
-}, refreshTimeInSec);
+//   let sensorModel = data.sensor_type;
+//   io.sockets.emit(sensorModel, data);
+// }, refreshTimeInSec);
 
-TSL2561.fetchInterval(function (err, data) {
-  if (err) {
-    console.error("An error occured: ", err.cause);
-    return;
-  }
+// TSL2561.fetchInterval(function (err, data) {
+//   if (err) {
+//     console.error("An error occured: ", err.cause);
+//     return;
+//   }
 
-  let sensorModel = data.sensor_type;
-  io.sockets.emit(sensorModel, data);
-}, refreshTimeInSec);
+//   let sensorModel = data.sensor_type;
+//   io.sockets.emit(sensorModel, data);
+// }, refreshTimeInSec);
 
 // let jsonObj = {
 //   type: 'Light',                                    // The type of the value of the sensor
