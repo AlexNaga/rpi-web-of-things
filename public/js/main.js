28320ef17c7dd9a09c7c15b9583a914ab4b3e5d7
AlexNaga <an222zd@student.lnu.se>
2018-03-02 17:41:45 +0100

    Import chart.js

--- public/js/main.js
+++ public/js/main.js
@@ -62,4 +62,23 @@ window.onload = function () {
     updateChart();
     xVal++
   }, updateInterval);
+
+
+  var ctx = document.getElementById("myChart").getContext('2d');
+
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      label: "Temperature via BMP180"
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
 }
