8adf38a8e50ed35944589dbfbf01ec630460b932
AlexNaga <an222zd@student.lnu.se>
2018-03-02 07:37:09 +0100

    Added sockets

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const http = require('http');
 const app = require('./app');
+const http = require('http');
+const io = require('socket.io')(http);
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
 
 server.listen(port);
