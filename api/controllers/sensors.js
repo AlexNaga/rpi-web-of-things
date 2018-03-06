e5b3894f5bc6931f3c86f7baaee440172dd140f4
AlexNaga <an222zd@student.lnu.se>
2018-03-06 08:36:34 +0100

    Get value from temp sensor

--- api/controllers/sensors.js
+++ api/controllers/sensors.js
@@ -1,4 +1,4 @@
-// const sensorLib = require('raspi-sensors');
+const sensorLib = require('raspi-sensors');
 
 // Lists all sensors
 exports.listSensors = (req, res, next) => {
@@ -17,60 +17,60 @@ exports.listSensors = (req, res, next) => {
       temperature: {
         href: process.env.DOMAIN + 'api/sensors/temperature',
         method: 'GET',
-        desc: 'Route for getting value from temperature sensor.'
+        desc: 'Route for getting current value from the temperature sensor.'
       },
       humidity: {
         href: process.env.DOMAIN + 'api/sensors/humidity',
         method: 'GET',
-        desc: 'Route for getting value from humidity sensor.'
+        desc: 'Route for getting current value from the humidity sensor.'
       },
       pressure: {
         href: process.env.DOMAIN + 'api/sensors/pressure',
         method: 'GET',
-        desc: 'Route for getting value from pressure sensor.'
+        desc: 'Route for getting current value from the pressure sensor.'
       },
       brightness: {
         href: process.env.DOMAIN + 'api/sensors/brightness',
         method: 'GET',
-        desc: 'Route for getting value from brightness sensor.'
+        desc: 'Route for getting current value from the brightness sensor.'
       }
     }
   });
 };
 
-// // Get value from temperature sensor
-// exports.getTemperature = (req, res, next) => {
-//   let DHT22 = new sensorLib.Sensor({
-//     type: 'DHT22',
-//     pin: 0X7
-//   }, 'temp_humidity_sensor');
+// Get value from temperature sensor
+exports.getTemperature = (req, res, next) => {
+  let DHT22 = new sensorLib.Sensor({
+    type: 'DHT22',
+    pin: 0X7
+  }, 'temp_humidity_sensor');
 
-//   DHT22.fetch(function (err, data) {
-//     if (err) {
-//       return res.status(500).json({
-//         message: 'An error occured!',
-//         error: err.cause
-//       }
-//     }
+  DHT22.fetch(function (err, data) {
+    if (err) {
+      return res.status(500).json({
+        message: 'An error occured!',
+        error: err.cause
+      }
+    }
 
-//     if (data.type === 'Temperature') {
-//       console.log(data);
-//       res.status(200).json({
-//         links: {
-//           sensors: {
-//             href: process.env.DOMAIN + 'api/sensors',
-//             method: 'GET',
-//             desc: 'Route for listing all sensors.'
-//           },
-//           temperature: {
-//             temperature: data
-//           }
-//         }
-//       });
-//     } else {
-//     }
-//   });
-// };
+    if (data.type === 'Temperature') {
+      console.log(data);
+      res.status(200).json({
+        links: {
+          sensors: {
+            href: process.env.DOMAIN + 'api/sensors',
+            method: 'GET',
+            desc: 'Route for listing all sensors.'
+          },
+          temperature: {
+            temperature: data
+          }
+        }
+      });
+    } else {
+    }
+  });
+};
 
 // let BMP180 = new sensorLib.Sensor({
 //   type: 'BMP180',
